74df7e03c3696937b59a6d2114db77132584b483
smilybarath07 <barathkumar6243@gmail.com>
2025-09-26 15:25:58 +0530

    My local changes before pulling

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Asminfotech/',
+  build:{
+    rollupOptions:{
+      input:{
+        main:'index.html',
+      }
+    }
+  },
   plugins: [react()],
 })
